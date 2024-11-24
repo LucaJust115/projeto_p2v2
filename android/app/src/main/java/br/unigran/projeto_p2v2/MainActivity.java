5c8f4d343604675de2792e9fcb408c5eeaf90d09
LucaJust115 <802.569@alunos.unigran.br>
2024-11-24 10:33:34 -0400

    Alteração do repositório Adição das dependencias necessarias Criação da tela de login

---
+++ android/app/src/main/java/br/unigran/projeto_p2v2/MainActivity.java
@@ -0,0 +1,6 @@
+package br.unigran.projeto_p2v2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/tela_login.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class TelaLogin extends StatefulWidget {
+  @override
+  _TelaLoginState createState() => _TelaLoginState();
+}
+
+class _TelaLoginState extends State<TelaLogin> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
+
+  Future<void> _login() async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _senhaController.text,
+      );
+      Navigator.pushNamed(context, '/home');  // Redirecionar para tela Home após login
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _senhaController,
+              decoration: InputDecoration(labelText: 'Senha'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: Text('Login'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/cadastro'); // Redirecionar para tela de cadastro
+              },
+              child: Text('Criar uma conta'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
